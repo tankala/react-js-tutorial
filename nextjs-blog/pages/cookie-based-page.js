2cf66308e8667d34bd0c686b23d759d25fab559a
tankala <ashok_tankala@yahoo.com>
2023-08-09 17:46:08 +0530

    testing with https url

--- nextjs-blog/pages/cookie-based-page.js
+++ nextjs-blog/pages/cookie-based-page.js
@@ -2,25 +2,26 @@ import Layout from '../components/layout';
 import Head from 'next/head';
 
 export const config = {
-    runtime: 'edge',
+    runtime: 'experimental-edge',
 };
 
-export default function Test({ response }) {
+export default function Test({ id }) {
     return (
         <Layout>
             <Head>
                 <title>Cookie Based Page</title>
             </Head>
             <div>
-                Got this response: {response}
+                Got this response: {id}
             </div>
         </Layout>
     );
 }
 
 export async function getServerSideProps(context) {
-    const cookieValue = context.req.cookies['cookie-data'];
-    let response = await fetch(`http://16.170.233.168:3000/api/cookie-based-response?cookieValue=${cookieValue}`);
+    // const cookieValue = context.req.cookies['cookie-data'];
+    // let response = await fetch(`http://16.170.233.168:3000/api/cookie-based-response?cookieValue=${cookieValue}`);
+    const response = await fetch('https://api.github.com/repos/vercel/next.js');
     const data = await response.json();
     return {
         props: data,
